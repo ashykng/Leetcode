be4c51078f61a83d41da30e5281c90acc79a792e
Ashkan Tavassoli <ashkant939@gmail.com>
2024-07-11 19:17:19 +0330

    problemset 9

---
+++ https-leetcode.com-problems-palindrome-numbe/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reversed = 0;
+        int original = x;
+        
+        if (x < 0)
+            return false;
+        
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        
+        return original == reversed;
+    }
+}
